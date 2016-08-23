4657848106ea779da6d4e13650db63331d53cade
Kenneth Gabbara <kenneth@eventhi.io>
2016-08-23 15:30:59 -0700

    ov

---
+++ redditalpha/static/js/eHIcolors.js
@@ -0,0 +1,15 @@
+/**
+ * Created by KG on 8/22/16.
+ */
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var pink = exports.pink = '#ec008c';
+var orange = exports.orange = '#f47d20';
+var purple = exports.purple = '#410b5f';
+var blue = exports.blue = '#00aeef';
+var black = exports.black = '#000000';
+var white = exports.white = '#ffffff';
