fc75e407bc36d7d69627b6855e8d09f34b02ebc4
Kenneth Gabbara <kenneth@eventhi.io>
2016-09-06 15:37:00 -0700

    fix cardlist

--- redditalpha/static/redditalpha/components/CardList.jsx
+++ redditalpha/static/redditalpha/components/CardList.jsx
@@ -1,27 +1,59 @@
+// import React from 'react';
+// import {GridList, GridTile} from 'material-ui/GridList';
+// import Card from './Card';
+//
+// const {Grid, Row, Col} = require('react-flexbox-grid');
+//
+// class CardList extends React.Component {
+//   render() {
+//     return (
+//       <Grid>
+//             {this.props.cards.map((card) => (
+//              <Card data={card} onClick={this.props.onClick}/>
+//             ))}
+//       </Grid>
+//     );
+//
+//     /*return (
+//       <div>
+//         <GridList
+//           cellHeight={200}
+//           padding={0}
+//           cols={8}
+//           style={{marginLeft:40}}
+//         >
+//           {this.props.cards.map((card) => (
+//             <GridTile key={card.id}>
+//               <Card data={card} onClick={this.props.onClick}/>
+//             </GridTile>
+//           ))}
+//         </GridList>
+//       </div>
+//     )*/
+//   }
+// }
+//
+// CardList.propTypes = {
+//   cards: React.PropTypes.array.isRequired,
+//   onClick: React.PropTypes.func.isRequired
+// };
+//
+// export default CardList;
+
+
 import React from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 import Card from './Card';
 
-const {Grid, Row, Col} = require('react-flexbox-grid');
 
 class CardList extends React.Component {
   render() {
     return (
-      <Grid>
-        <Row>
-            {this.props.cards.map((card) => (
-              <Col xs={6} sm={4} md={2} lg={1}><Card data={card} onClick={this.props.onClick}/></Col>
-            ))}
-        </Row>
-      </Grid>
-    );
-
-    /*return (
       <div>
         <GridList
           cellHeight={200}
           padding={0}
-          cols={8}
+          cols={12}
           style={{marginLeft:40}}
         >
           {this.props.cards.map((card) => (
@@ -31,7 +63,7 @@ class CardList extends React.Component {
           ))}
         </GridList>
       </div>
-    )*/
+    )
   }
 }
 
