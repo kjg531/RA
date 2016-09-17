dcafe47cf7fb27bbd359aa9512612fefb2b32a17
aaj <mando.alvarado.jose@gmail.com>
2016-09-17 17:25:30 -0600

    Gunicorn settings

---
+++ config/gunicorn.config.py
@@ -0,0 +1,12 @@
+bind = "0.0.0.0:32181"
+
+workers = 2
+
+max_requests = 1000
+timeout = 30
+keep_alive = 2
+
+preload = True
+
+accesslog = '../../logs/access.log'
+errorlog = '../../logs/error.log'
